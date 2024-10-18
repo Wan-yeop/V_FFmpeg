25397c12cfb94a061520e2a3d886dd297d8d63d1
Wan-yeop <wyan33@gmail.com>
2024-10-18 13:05:46 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace V_FFmpeg
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.New_Window = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // New_Window
+            // 
+            this.New_Window.Location = new System.Drawing.Point(59, 38);
+            this.New_Window.Name = "New_Window";
+            this.New_Window.Size = new System.Drawing.Size(126, 40);
+            this.New_Window.TabIndex = 0;
+            this.New_Window.Text = "Capture Size";
+            this.New_Window.UseVisualStyleBackColor = true;
+            this.New_Window.Click += new System.EventHandler(this.New_Window_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(59, 127);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(126, 34);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(59, 214);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(126, 41);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 449);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.New_Window);
+            this.HelpButton = true;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button New_Window;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace V_FFmpeg
+{
+    public partial class MainForm : Form
+    {
+        private Form childForm;   //동적 생성 자식 폼
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        // New Windows-----------------------------------------------
+        private void New_Window_Click(object sender, EventArgs e)
+        {
+            childForm = new Form
+            {
+                Size = new Size(400, 300),
+                BackColor = Color.Red,
+                StartPosition = FormStartPosition.CenterScreen,
+                Opacity = 0.6
+            };
+
+            childForm.Move += ChildForm_MoveResize;     // + move event 
+            childForm.Resize += ChildForm_MoveResize;   // + resize event
+
+            childForm.Show();
+
+        }
+
+        // Windows Location & Size
+        private void ChildForm_MoveResize(Object sender, EventArgs e)
+        {
+            string xy;
+
+            xy = " X:" + childForm.Left.ToString()
+                + "_Y:" + childForm.Top.ToString()
+                + " W:" + childForm.Width.ToString()
+                + "_H:" + childForm.Height.ToString();
+
+            childForm.Text = xy;
+        }
+    }
+}
