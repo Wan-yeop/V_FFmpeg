3a0d98a521036a5824a24c913a84632cbbd34289
Wan-yeop <wyan33@gmail.com>
2024-10-19 11:54:18 +0900

    2024.10.10

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,45 +36,50 @@ namespace V_FFmpeg
             // 
             // New_Window
             // 
-            this.New_Window.Location = new System.Drawing.Point(59, 38);
+            this.New_Window.Location = new System.Drawing.Point(74, 46);
+            this.New_Window.Margin = new System.Windows.Forms.Padding(4);
             this.New_Window.Name = "New_Window";
-            this.New_Window.Size = new System.Drawing.Size(126, 40);
+            this.New_Window.Size = new System.Drawing.Size(184, 48);
             this.New_Window.TabIndex = 0;
-            this.New_Window.Text = "Capture Size";
+            this.New_Window.Text = "Capture 영역 지정";
             this.New_Window.UseVisualStyleBackColor = true;
             this.New_Window.Click += new System.EventHandler(this.New_Window_Click);
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(59, 127);
+            this.btnStart.Location = new System.Drawing.Point(74, 152);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(126, 34);
+            this.btnStart.Size = new System.Drawing.Size(158, 41);
             this.btnStart.TabIndex = 1;
-            this.btnStart.Text = "Start";
+            this.btnStart.Text = "Start Recording";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(59, 214);
+            this.btnStop.Location = new System.Drawing.Point(74, 256);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(126, 41);
+            this.btnStop.Size = new System.Drawing.Size(158, 49);
             this.btnStop.TabIndex = 2;
-            this.btnStop.Text = "Stop";
+            this.btnStop.Text = "Stop Recording";
             this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(566, 449);
+            this.ClientSize = new System.Drawing.Size(708, 539);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.New_Window);
             this.HelpButton = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Main Form";
+            this.Text = "Screen Recording  Ver0.9";
             this.ResumeLayout(false);
 
         }
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace V_FFmpeg
@@ -13,6 +11,12 @@ namespace V_FFmpeg
     public partial class MainForm : Form
     {
         private Form childForm;   //동적 생성 자식 폼
+        private Process ffmpegProcess;
+        private StreamWriter ffmpegInput;
+        private bool ffmpegRunning = false;
+
+        private Rectangle childFormRetangle;
+        private String ffmpegArguments;
 
         public MainForm()
         {
@@ -22,11 +26,103 @@ namespace V_FFmpeg
         private void btnStart_Click(object sender, EventArgs e)
         {
 
+            if (childForm != null)
+            {
+                childForm.Close();
+                childForm = null;
+            }
+
+            if (ffmpegArguments == null)
+            {
+                MessageBox.Show("먼저 Capture 영역을 지정해주세요");
+                return;
+            }
+
+            //string commandLine = " -y -f gdigrab -framerate 30 -offset_x 10 -offset_y 10 -video_size 400x400 -i desktop -vcodec libx264 -pix_fmt yuv420p output.mp4";
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "C:\\msys64\\ffmpeg_src\\ffmpeg\\ffmpeg2.exe",   //"C:\\msys64\\ffmpeg_src\\ffmpeg\\ffmpeg2.exe", "C:\\msys64\\mingw64\\bin\\ffmpeg.exe"
+                Arguments = ffmpegArguments,
+                RedirectStandardInput = true,
+                RedirectStandardError = false,
+                RedirectStandardOutput = false,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+
+            ffmpegProcess = new Process { StartInfo = startInfo };
+
+            try
+            {
+                ffmpegProcess.Start();
+                ffmpegInput = ffmpegProcess.StandardInput;  //입력 스트림 생성
+                ffmpegRunning = true;
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+
+                MessageBox.Show("FFmpeg recording started.");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Faild to start FFmpeg: " + ex.Message);
+            }
+
+
+        }
+
+        //STOP Button Click !!
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (!ffmpegRunning)
+            {
+                MessageBox.Show("FFmpeg is not running.");
+                return;
+            }
+
+            try
+            {
+                // 'q' 명령어 전달
+
+                byte[] exitCommand = new byte[] { (byte)'q' };  // 'q'의 ASCII 값
+                ffmpegProcess.StandardInput.BaseStream.Write(exitCommand, 0, 1);  // 1바이트 전송
+                ffmpegProcess.StandardInput.BaseStream.Flush();  // 버퍼 플러시
+
+
+                if (!ffmpegProcess.WaitForExit(5000))  //프로세스 종료 대기,.
+                {
+                    ffmpegProcess.Kill();
+                    MessageBox.Show("FFmpeg was forcefully terminated.");
+                }
+                else
+                {
+                    MessageBox.Show("FFmpeg recording stopped normally");
+                }
+
+                ffmpegProcess.Close();
+
+                ffmpegRunning = false;
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+
+
+                ffmpegArguments = null;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Faild to Stop FFmpeg" + ex.Message);
+            }
+
         }
 
 
 
-        // New Windows-----------------------------------------------
+        // New Windows Button Click !!
+
         private void New_Window_Click(object sender, EventArgs e)
         {
             childForm = new Form
@@ -34,23 +130,38 @@ namespace V_FFmpeg
                 Size = new Size(400, 300),
                 BackColor = Color.Red,
                 StartPosition = FormStartPosition.CenterScreen,
-                Opacity = 0.6
+                Opacity = 0.7
             };
 
             childForm.Move += ChildForm_MoveResize;     // + move event 
             childForm.Resize += ChildForm_MoveResize;   // + resize event
+            childForm.FormClosing += ChildForm_FormClosing;
 
             childForm.Show();
 
         }
 
+        // 캡쳐 영역 창을 닫기 전에 위치 및 사이즈 정보 얻기 (ffmpegArguments)
+        private void ChildForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // H264는  width, Height 가 2의 배수여야 한다. -vf "scale="
+            Rectangle Rect = new Rectangle(childForm.Left+10, childForm.Top+10, (int)(Math.Truncate(childForm.Width/2.0)*2), (int)(Math.Truncate(childForm.Height/2.0)*2) );
+
+            ffmpegArguments = $"-y -f gdigrab -framerate 30 -offset_x {Rect.Left} -offset_y {Rect.Top} -video_size {Rect.Width}x{Rect.Height} -i desktop -vcodec libx264 -pix_fmt yuv420p output.mp4";
+        }
+
         // Windows Location & Size
         private void ChildForm_MoveResize(Object sender, EventArgs e)
         {
-            string xy;
+            if ( childForm.Left < 0 )
+            {
+                childForm.Left = 0;  // 창 Left 값이 0 이하가 되지 않도록
+            }
 
-            xy = " X:" + childForm.Left.ToString()
-                + "_Y:" + childForm.Top.ToString()
+            String xy;
+
+            xy = " X:" + (childForm.Left+10).ToString()
+                + "_Y:" + (childForm.Top+10).ToString()
                 + " W:" + childForm.Width.ToString()
                 + "_H:" + childForm.Height.ToString();
 
@@ -58,3 +169,4 @@ namespace V_FFmpeg
         }
     }
 }
+   
